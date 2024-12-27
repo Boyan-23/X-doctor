c72c360194a8e1f4a2b8c5e3578fc04ecaaaf68a
X Doctor <hr15350@hotmail.com>
2024-12-27 17:44:15 +0800

    This is first commit, and is an example.

---
+++ Example26_DSP2833x_SPI_loopback/Debug/APP/spi/subdir_vars.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../APP/spi/spi.c 
+
+OBJS += \
+./APP/spi/spi.obj 
+
+C_DEPS += \
+./APP/spi/spi.d 
+
+C_DEPS__QUOTED += \
+"APP\spi\spi.d" 
+
+OBJS__QUOTED += \
+"APP\spi\spi.obj" 
+
+C_SRCS__QUOTED += \
+"../APP/spi/spi.c" 
+
+
---
+++ Example26_DSP2833x_SPI_loopback/Debug/DSP2833x_Libraries/subdir_rules.mk
@@ -0,0 +1,104 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+DSP2833x_Libraries/DSP2833x_ADC_cal.obj: ../DSP2833x_Libraries/DSP2833x_ADC_cal.asm $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_ADC_cal.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Libraries/DSP2833x_Adc.obj: ../DSP2833x_Libraries/DSP2833x_Adc.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_Adc.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Libraries/DSP2833x_CodeStartBranch.obj: ../DSP2833x_Libraries/DSP2833x_CodeStartBranch.asm $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_CodeStartBranch.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Libraries/DSP2833x_CpuTimers.obj: ../DSP2833x_Libraries/DSP2833x_CpuTimers.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_CpuTimers.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Libraries/DSP2833x_DefaultIsr.obj: ../DSP2833x_Libraries/DSP2833x_DefaultIsr.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_DefaultIsr.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Libraries/DSP2833x_EPwm.obj: ../DSP2833x_Libraries/DSP2833x_EPwm.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_EPwm.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Libraries/DSP2833x_GlobalVariableDefs.obj: ../DSP2833x_Libraries/DSP2833x_GlobalVariableDefs.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_GlobalVariableDefs.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Libraries/DSP2833x_Gpio.obj: ../DSP2833x_Libraries/DSP2833x_Gpio.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_Gpio.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Libraries/DSP2833x_PieCtrl.obj: ../DSP2833x_Libraries/DSP2833x_PieCtrl.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_PieCtrl.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Libraries/DSP2833x_PieVect.obj: ../DSP2833x_Libraries/DSP2833x_PieVect.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_PieVect.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Libraries/DSP2833x_Sci.obj: ../DSP2833x_Libraries/DSP2833x_Sci.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_Sci.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Libraries/DSP2833x_Spi.obj: ../DSP2833x_Libraries/DSP2833x_Spi.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_Spi.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Libraries/DSP2833x_SysCtrl.obj: ../DSP2833x_Libraries/DSP2833x_SysCtrl.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_SysCtrl.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+DSP2833x_Libraries/DSP2833x_usDelay.obj: ../DSP2833x_Libraries/DSP2833x_usDelay.asm $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/spi" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/uart" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/time" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_common/include" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Example/Example26_DSP2833x_SPI_loopback/APP/leds" --include_path="E:/DSP8233x_ProjectExample/DSP2833x_Libraries/DSP2833x_headers/include" --include_path="C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_15.12.1.LTS/include" -g --diag_warning=225 --display_error_number --diag_wrap=off --preproc_with_compile --preproc_dependency="DSP2833x_Libraries/DSP2833x_usDelay.d" --obj_directory="DSP2833x_Libraries" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
